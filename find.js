5d3befa41182ae34a45d34454276121c0babb523
aysha3f <tahiaysha220@gmail.com>
2025-07-28 22:08:10 +0600

    foreach, filter, find, and differences between them

---
+++ find.js
@@ -0,0 +1,14 @@
+// node find.js
+const players = [75, 65, 67, 72, 55, 59];
+
+// const selected = players.find(player => player >70);
+// const selected = players.find(player => player >80);
+const selected = players.find(player => player >50);
+console.log(selected);
+
+/**
+ * 1. map
+ * 2. forEach
+ * 3. filter
+ * 4. find
+ */
